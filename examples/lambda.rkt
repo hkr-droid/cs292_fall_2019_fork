4a05035bd985fe47502b6ccbebd67732bd02cec2
Zach Sisco <zach@zsisco.net>
2019-11-16 11:55:31 -0800

    Defines simpl: a scheme interpreter for an arithmetic DSL with variables; also add example scheme interpreters from Matt Might

---
+++ examples/lambda.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+; This is the lambda calculus interpreter written by Matt Might
+
+; bring in the match library:
+(require racket/match)
+
+; eval matches on the type of expression:
+(define (eval exp env) (match exp
+  [`(,f ,e)        (apply (eval f env) (eval e env))]
+  [`(λ ,v . ,e)   `(closure ,exp ,env)]
+  ;[`(add e1 e2)    (
+  [(? symbol?)     (cadr (assq exp env))]
+  ;[(? number?)      exp]
+))
+
+; apply destructures the function with a match too:
+(define (apply f x) (match f
+  [`(closure (λ ,v . ,body) ,env)
+    (eval body (cons `(,v ,x) env))]))
+
+; read in, parse and evaluate:
+(display (eval (read) '()))    (newline)
+
+;(λ x . x)
+;(((λ f . (λ x . (f x))) (λ a . a)) (λ b . b))
+;(λ x . (+ x 1))
---
+++ examples/simpl.rkt
@@ -0,0 +1,57 @@
+#lang rosette
+
+(require rosette/lib/angelic)
+(require rosette/lib/match)
+
+; syntax for expressions
+(struct add (left right) #:transparent)
+(struct sub (left right) #:transparent)
+(struct mul (left right) #:transparent)
+
+; evaluate expressions (result is number)
+(define (eval exp env)
+  (match exp
+    [(? symbol?)    (hash-ref env exp)]
+    [(? number?)     exp]
+    [(add a b)      (+ (eval a env) (eval b env))]
+    [(sub a b)      (- (eval a env) (eval b env))]
+    [(mul a b)      (* (eval a env) (eval b env))]
+))
+
+; empty environment
+(define (env-empty)  (hash))
+
+; example
+(define exp1 (sub (add 7 4) 'a))
+(define env (hash))
+
+; evaluate exp1: (7 + 4) - a = 6
+(eval exp1 (hash-set env 'a 5))
+
+; syntax for commands
+(struct skip () #:transparent)
+(struct := (left right) #:transparent)
+(struct seq (c1 c2) #:transparent)
+
+; interpret commands (returns resulting environment)
+(define (interpret com env)
+  (match com
+    [(skip)          env]
+    [(:= v e)       (hash-set env v (eval e env))]
+    [(seq c1 c2)    (interpret c2 (interpret c1 env))]
+))
+
+; example program
+(define com1 (seq (:= 'a 5) (:= 'b exp1)))
+
+; runs the following program:
+;   a := 5
+;   b := (7 + 4) - a
+; Result should be a state with { a -> 5 and b -> 6 }
+(interpret com1 (env-empty))
+
+;(interpret (seq (skip) (seq (skip) com1)) (env-empty))
+
+; to do: add rosette symbolics
+;(define-symbolic c x y integer?)
+;(eval (mul x (add y 2)))
