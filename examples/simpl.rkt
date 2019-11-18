0be2dba14d3478cc3edee21b6365c8f859a743d9
Zach Sisco <zach@zsisco.net>
2019-11-17 17:32:52 -0800

    change seq to : operator

--- examples/simpl.rkt
+++ examples/simpl.rkt
@@ -29,20 +29,24 @@
 (eval exp1 (hash-set env 'a 5))
 
 ; syntax for commands
-(struct skip () #:transparent)
-(struct := (left right) #:transparent)
-(struct seq (c1 c2) #:transparent)
+(struct skip () #:transparent)         ; skip (no-op)
+(struct := (left right) #:transparent) ; assignment
+(struct : (c1 c2) #:transparent)       ; sequence commands
+;(struct load (g v) #:transparent)      ; load a global variable into a local variable value
+;(struct store (v g) #:transparent)     ; store a global variable with a local variable value
 
 ; interpret commands (returns resulting environment)
 (define (interpret com env)
   (match com
-    [(skip)          env]
-    [(:= v e)       (hash-set env v (eval e env))]
-    [(seq c1 c2)    (interpret c2 (interpret c1 env))]
+    [(skip)             env]
+    [(:= v e)          (hash-set env v (eval e env))]
+    [(: c1 c2)         (interpret c2 (interpret c1 env))]
+    ;[(load g v)      env]
+    ;[(store v g)     env]
 ))
 
 ; example program
-(define com1 (seq (:= 'a 5) (:= 'b exp1)))
+(define com1 (: (:= 'a 5) (:= 'b exp1)))
 
 ; runs the following program:
 ;   a := 5
@@ -50,7 +54,23 @@
 ; Result should be a state with { a -> 5 and b -> 6 }
 (interpret com1 (env-empty))
 
-;(interpret (seq (skip) (seq (skip) com1)) (env-empty))
+(interpret (: (skip) (: (skip) com1)) (env-empty))
+
+; syntax is ugly as sin, but if you write it like so
+; it becomes a little bit more tolerable, and it looks so happy! (:
+(define com2 (: (:= 'a 5)
+             (: (:= 'b 50)
+             (: (:= 'c (add 'a 'b))
+                (skip)))))
+(interpret com2 (env-empty))
+
+(define com3 (: (:= 'a 5)
+             (: (:= 'b 50)
+             (: (:= 'a (add 'a 1))
+             (: (:= 'b (add 'b 1))
+             (: (:= 'b (add 'b 1))
+                (skip)))))))
+(interpret com3 (env-empty))
 
 ; to do: add rosette symbolics
 ;(define-symbolic c x y integer?)
