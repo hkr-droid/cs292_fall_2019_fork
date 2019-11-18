6ad27af09863f9e5679f6cf7cfa28d1902388b6b
Zach Sisco <zach@zsisco.net>
2019-11-17 18:07:33 -0800

    add boolean expressions and if-then-else commands

--- examples/simpl.rkt
+++ examples/simpl.rkt
@@ -3,47 +3,58 @@
 (require rosette/lib/angelic)
 (require rosette/lib/match)
 
-; syntax for expressions
+; syntax for arithmetic expressions
 (struct add (left right) #:transparent)
 (struct sub (left right) #:transparent)
 (struct mul (left right) #:transparent)
 
-; evaluate expressions (result is number)
-(define (eval exp env)
+; evaluate arithmetic expressions (result is number)
+(define (aeval exp env)
   (match exp
     [(? symbol?)    (hash-ref env exp)]
     [(? number?)     exp]
-    [(add a b)      (+ (eval a env) (eval b env))]
-    [(sub a b)      (- (eval a env) (eval b env))]
-    [(mul a b)      (* (eval a env) (eval b env))]
+    [(add a b)      (+ (aeval a env) (aeval b env))]
+    [(sub a b)      (- (aeval a env) (aeval b env))]
+    [(mul a b)      (* (aeval a env) (aeval b env))]
 ))
 
-; empty environment
-(define (env-empty)  (hash))
+; syntax for boolean expressions
+(struct beq  (a b) #:transparent)
+(struct ble  (a b) #:transparent)
+(struct bnot (a)   #:transparent)
+(struct band (a b) #:transparent)
 
-; example
-(define exp1 (sub (add 7 4) 'a))
-(define env (hash))
-
-; evaluate exp1: (7 + 4) - a = 6
-(eval exp1 (hash-set env 'a 5))
+; evaluate boolean expressions (result is boolean)
+(define (beval exp env)
+  (match exp
+    [(? boolean?)    exp]
+    [(beq a b)       (= (aeval a env) (aeval b env))]
+    [(ble a b)       (<= (aeval a env) (aeval b env))]
+    [(bnot a)        (not (beval a env))]
+    [(band a b)      (and (beval a env) (beval b env))]
+))
 
 ; syntax for commands
-(struct skip () #:transparent)         ; skip (no-op)
-(struct := (left right) #:transparent) ; assignment
-(struct : (c1 c2) #:transparent)       ; sequence commands
+(struct skip ()          #:transparent) ; skip (no-op)
+(struct :=  (left right) #:transparent) ; assignment
+(struct :   (c1 c2)      #:transparent) ; sequence commands
+(struct ite (e c1 c2)    #:transparent) ; if then else
 ;(struct load (g v) #:transparent)      ; load a global variable into a local variable value
 ;(struct store (v g) #:transparent)     ; store a global variable with a local variable value
 
 ; interpret commands (returns resulting environment)
 (define (interpret com env)
   (match com
-    [(skip)             env]
-    [(:= v e)          (hash-set env v (eval e env))]
+    [(skip)            env]
+    [(:= v e)          (hash-set env v (aeval e env))]
     [(: c1 c2)         (interpret c2 (interpret c1 env))]
+    [(ite e c1 c2)     (if (beval e env) (interpret c1 env) (interpret c2 env))]
     ;[(load g v)      env]
     ;[(store v g)     env]
 ))
+
+; empty environment
+(define (env-empty)  (hash))
 
 ; example program
 (define com1 (: (:= 'a 5) (:= 'b exp1)))
@@ -52,9 +63,12 @@
 ;   a := 5
 ;   b := (7 + 4) - a
 ; Result should be a state with { a -> 5 and b -> 6 }
+(printf "Test 1:~n a := 5 ; b := (7 + 4) - a ~n")
 (interpret com1 (env-empty))
-
+; bunch of skips makes no difference
+(printf " skip ; skip ; a := 5 ; b := (7 + 4) - a ~n")
 (interpret (: (skip) (: (skip) com1)) (env-empty))
+(printf "~n")
 
 ; syntax is ugly as sin, but if you write it like so
 ; it becomes a little bit more tolerable, and it looks so happy! (:
@@ -62,7 +76,9 @@
              (: (:= 'b 50)
              (: (:= 'c (add 'a 'b))
                 (skip)))))
+(printf "Test 2:~n a := 5 ; b := 50 ; c := a + b ; skip ~n")
 (interpret com2 (env-empty))
+(printf "~n")
 
 (define com3 (: (:= 'a 5)
              (: (:= 'b 50)
@@ -70,8 +86,22 @@
              (: (:= 'b (add 'b 1))
              (: (:= 'b (add 'b 1))
                 (skip)))))))
+(printf "Test 3:~n a := 5 ; b := 50 ; a := a + 1; b := b + 1 ; b := b + 1 ; skip ~n")
 (interpret com3 (env-empty))
+(printf "~n")
+
+; if-then-else test
+(define com4true (: (:= 'a 5)
+                 (: (:= 'b 50)
+                    (ite (and (ble 'a 'b) (beq (mul 'a 10) 'b)) (:= 'a (add 'a 1)) (:= 'b (add 'b 1))))))
+(printf "Test 4 true branch:~n a := 5 ; b := 50 ; if a <= b and (a * 10 = b) then a := a + 1 else b := b + 1 ~n")
+(interpret com4true (env-empty))
+(define com4false (: (:= 'a 5)
+                  (: (:= 'b 50)
+                     (ite (bnot (ble 'a 'b)) (:= 'a (add 'a 1)) (:= 'b (add 'b 1))))))
+(printf "Test 4 false branch:~n a := 5 ; b := 50 ; if !(a <= b) then a := a + 1 else b := b + 1 ~n")
+(interpret com4false (env-empty))
 
 ; to do: add rosette symbolics
 ;(define-symbolic c x y integer?)
-;(eval (mul x (add y 2)))
+;(aeval (mul x (add y 2)))
