a1130d1dcbe432c580b00af5f7e2522291f1dae2
Zach Sisco <zach@zsisco.net>
2019-11-26 10:26:39 -0800

    fix match syntax, implement assign ite while et al

--- examples/minilang_small.rkt
+++ examples/minilang_small.rkt
@@ -2,6 +2,21 @@
 
 (require racket/match racket/struct)
 
+;; env is a map of maps
+;; given a tag, look up the map for that store 
+;; (e.g. 'thread1, 'global, 'semaphore)
+;; then return the value for the given key
+(define (lookup-env env store key)
+  (hash-ref (hash-ref env store) key))
+
+;; given an environment, store, key, value, 
+;; look up the map for the given tag, 
+;; and set val for the given key
+(define (set-env env store key val)
+  (hash-set env store
+            (hash-set (hash-ref env store) key val)))
+
+;; syntax
 (struct add (e1 e2) #:transparent)
 (struct sub (e1 e2) #:transparent)
 (struct mul (e1 e2) #:transparent)
@@ -15,8 +30,11 @@
 (struct skip () #:transparent)
 (struct := (left right) #:transparent)
 (struct : (c1 c2) #:transparent)
+(struct ite (b c1 c2) #:transparent)
+(struct while (b c) #:transparent)
 (struct 止 () #:transparent)  ;; as in 止まれ (tomare), halt
 
+;; state struct with pretty printing
 (struct state (c e k)
   #:transparent
   #:methods gen:custom-write
@@ -25,63 +43,72 @@
       (lambda (obj) 'state)
       (lambda (obj) (list (state-c obj) (state-e obj) (state-k obj)))))])
 
+;; step function
 (define (step s)
   (match s
-    
-    [(state (cons (? number? n) c) e k)         (state c e (cons n k))]
-    [(state (cons (? boolean? b) c) e k)        (state c e (cons b k))]
-    
-    ;[(?symbol? e k)	                         (state (env-lookup ?symbol? e) e k)]  ;; todo ensure lookup works
+
+    ;; values
+    [(state (list-rest (? number? n) c) e k)            (state c e (cons n k))]
+    [(state (list-rest (? boolean? b) c) e k)           (state c e (cons b k))]
 
     ;; arithmetic operations
-    [(state (cons `(add ,E1 ,E2) c) e k)            (state (cons E1 (cons E2 (cons '+ c))) e k)]
-    [(state (cons `(sub ,E1 ,E2) c) e k)            (state (cons E1 (cons E2 (cons '- c))) e k)]
-    [(state (cons `(mul ,E1 ,E2) c) e k)            (state (cons E1 (cons E2 (cons '* c))) e k)]
+    [(state (list-rest (add E1 E2) c) e k)              (state (cons E1 (cons E2 (cons '+ c))) e k)]
+    [(state (list-rest (sub E1 E2) c) e k)              (state (cons E1 (cons E2 (cons '- c))) e k)]
+    [(state (list-rest (mul E1 E2) c) e k)              (state (cons E1 (cons E2 (cons '* c))) e k)]
     
-    [(state (cons '+ c) e (cons n2 (cons n1 k)))   (state c e (cons (+ n1 n2) k))]
-    [(state (cons '- c) e (cons n2 (cons n1 k)))   (state c e (cons (- n1 n2) k))]
-    [(state (cons '* c) e (cons n2 (cons n1 k)))   (state c e (cons (* n1 n2) k))]
+    [(state (list-rest '+ c) e (cons n2 (cons n1 k)))   (state c e (cons (+ n1 n2) k))]
+    [(state (list-rest '- c) e (cons n2 (cons n1 k)))   (state c e (cons (- n1 n2) k))]
+    [(state (list-rest '* c) e (cons n2 (cons n1 k)))   (state c e (cons (* n1 n2) k))]
     
     ;; boolean operations
-    [(state (cons `(¬ ,B) c) e k)                   (state (cons B (cons '¬ c)) e k)]
-    [(state (cons `(∧ ,B1 ,B2) c) e k)              (state (cons B1 (cons B2 (cons '∧ c))) e k)]
+    [(state (list-rest (¬ B) c) e k)                    (state (cons B (cons '¬ c)) e k)]
+    [(state (list-rest (∧ B1 B2) c) e k)                (state (cons B1 (cons B2 (cons '∧ c))) e k)]
 
-    [(state (cons '¬ c) e (cons b k))                (state c e (cons (not b) k))]
-    [(state (cons '∧ c) e (cons b2 (cons b1 k)))     (state c e (cons (and b1 b2) k))]
+    [(state (list-rest '¬ c) e (cons b k))              (state c e (cons (not b) k))]
+    [(state (list-rest '∧ c) e (cons b2 (cons b1 k)))   (state c e (cons (and b1 b2) k))]
 
-    [(state (cons `(=? ,E1 ,E2) c) e k)             (state (cons E1 (cons E2 (cons '=? c))) e k)]
-    [(state (cons `(≤ ,E1 ,E2) c) e k)              (state (cons E1 (cons E2 (cons '≤ c))) e k)]
+    [(state (list-rest (=? E1 E2) c) e k)               (state (cons E1 (cons E2 (cons '=? c))) e k)]
+    [(state (list-rest (≤ E1 E2) c) e k)                (state (cons E1 (cons E2 (cons '≤ c))) e k)]
     
-    [(state (cons '=? c) e (cons n2 (cons n1 k)))    (state c e (cons (= n1 n2) k))]
-    [(state (cons '≤ c) e (cons n2 (cons n1 k)))     (state c e (cons (<= n1 n2) k))]
+    [(state (list-rest '=? c) e (cons n2 (cons n1 k)))  (state c e (cons (= n1 n2) k))]
+    [(state (list-rest '≤ c) e (cons n2 (cons n1 k)))   (state c e (cons (<= n1 n2) k))]
 
-    [(state (cons `(skip) c) e k)                   (state c e k)]
+    ;; commands
+    [(state (list-rest (skip) c) e k)                   (state c e k)]
     
-    [(state (cons `(: ,C1 ,C2) c) e k)              (state (cons C1 (cons C2 c)) e k)]
-    
-    
-    ;[((list-ref skip c) e k)                          (state c e k)
-    ;[((list-ref (ite b c0 c1) c) e k)                 (state (cons b (cons ite c)) e (cons c0 (cons c1 k)))]
-    ;[((list-ref ite c) e (list-ref #t c0 c1 k) )    (state c0 e k)]
-    ;[((list-ref ite c) e (list-ref #f c0 c1 k) )   (state c1 e k)]
-    ;[((list-ref follows c0 c1 c) e k)	              (state (cons c0 (cons c1 c)) e k)]
-    ;; todo [(assign ,v ,e)	          (env-set! env v e)]
-    ;; todo while
+    [(state (list-rest (: C1 C2) c) e k)                (state (cons C1 (cons C2 c)) e k)]
 
-    [(state (cons `(止) _) e k)                  (state '() e k)] 
+    [(state (list-rest (:= x E) c) e k)                 (state (cons E (cons ':= c)) e (cons x k))]
+    [(state (list-rest ':= c) e (cons n (cons x k)))    (state c (set-env e 't1 x n) k)]
+
+    [(state (list-rest (ite B C1 C2) c) e k)                       (state (cons B (cons 'ite c)) e (cons C1 (cons C2 k)))]
+    [(state (list-rest 'ite c) e (cons #t (cons C1 (cons C2 k))))  (state (cons C1 c) e k)]
+    [(state (list-rest 'ite c) e (cons #f (cons C1 (cons C2 k))))  (state (cons C2 c) e k)]
+
+    [(state (list-rest (while B C) c) e k)                         (state (cons B (cons 'while c)) e (cons B (cons C k)))]
+    [(state (list-rest 'while c) e (cons #t (cons B (cons C k))))  (state (cons C (cons (while B C) c)) e k)]
+    [(state (list-rest 'while c) e (cons #f (cons B (cons C k))))  (state c e k)]
+    
+    ;; todo load / store (global data)
+    
+    [(state (list-rest (? symbol? x) c) e k)	        (state c e (cons (lookup-env e 't1 x) k))] 
+    [(state (list-rest (止) _) e k)                    (state '() e k)] 
 ))
 
-(step (state '((止)) (list) (list))) 
+(define t1-env
+  (hash-set (hash) 't1 (hash)))
 
-(define p1 '(1 (止)))
-(step (step (state p1 (list) (list))))
+;(step (state '((止)) t1-env (list))) 
 
-(define p2 '( (add 1 2) ))
-(define s (state p2 (list) (list)))
-(step s)
-(step (step s))
-(step (step (step s)))
-(step (step (step (step s))))
+;(define p1 '(1 (止)))
+;(step (step (state p1 t1-env (list))))
+
+;(define p2 '( (add 1 2) ))
+;(define s (state p2 t1-env (list)))
+;(step s)
+;(step (step s))
+;(step (step (step s)))
+;(step (step (step (step s))))
 
 (define (run s)
   (begin
@@ -89,23 +116,58 @@
     (display "\n-->\n")
     (let [(next (step s))]
       (match next
-        [(state (cons '(止) _) _ _)
+        [(state (cons (止) _) _ (list))  ;; done when state is < 止, e, () >
          (display next)
-         (display "\ndone\n")]
+         (display "\ndone\n\n")]
         [else  (run next)]))))
 
-(run (state '( (sub 6 4) (止)) (list) (list)))
+;(run (state (list 'a (止)) (set-env t1-env 't1 'a 6) (list)))
 
-(run (state '( (mul (sub 6 4) 3) (止)) (list) (list)))
+;(run (state (list (add 'a 4) (止)) (set-env t1-env 't1 'a 6) (list)))
 
-(run (state '( (≤ (mul (sub 6 4) 3) (add -1 -2)) (止)) (list) (list)))
+;(run (state (list (:= 'a (mul (sub 6 4) 3)) (止)) t1-env (list)))
 
-(run (state '( (=? (mul -1 3) (add -1 -2)) (止)) (list) (list)))
+;(run (state (list (≤ (mul (sub 6 4) 3) (add -1 -2)) (止)) t1-env (list)))
 
-(run (state '( (¬ (=? (mul -1 3) (add -1 -2))) (止)) (list) (list)))
+;(run (state (list (=? (mul -1 3) (add -1 -2)) (止)) t1-env (list)))
 
-(run (state '( (∧ (≤ (add -1 -2) (mul (sub 6 4) 3)) (=? (mul -1 3) (add -1 -2))) (止)) (list) (list)))
+;(run (state (list (¬ (=? (mul -1 3) (add -1 -2))) (止)) t1-env (list)))
 
-(run (state '( (skip) (sub 6 4) (止)) (list) (list)))
+;(run (state (list (∧ (≤ (add -1 -2) (mul (sub 6 4) 3)) (=? (mul -1 3) (add -1 -2))) (止)) t1-env (list)))
 
-(run (state '( (: (skip) (sub 6 4)) (止)) (list) (list)))
+;(run (state (list (:= 'a 1) (止) ) t1-env (list)))
+
+;(run (state (list (:= 'a 1) (止) ) (set-env t1-env 't1 'a 6) (list)))
+
+;(run (state (list (: (:= 'a 1) (:= 'b (add 'a 10))) (止)) t1-env (list)))
+;; do we need follows? :? yes, because if/while body
+;(run (state (list (:= 'a 1) (skip) (:= 'b (add 'a 10)) (止)) t1-env (list)))
+
+;(run (state (list (ite (≤ (mul (sub 6 4) 3) (add -1 -2)) (:= 'a 0) (:= 'a -1)) (止)) t1-env (list)))
+
+(run (state (list 
+                  (: (:= 'a 2)
+                  (: (ite (=? 'a 2)
+                          (: (:= 'a (add 'a 1))
+                             (:= 'a (add 'a 1)))
+                          (:= 'b 2))
+                      (止))))
+                   t1-env (list)))
+
+(run (state (list 
+                  (: (:= 'a 3)
+                  (: (ite (=? 'a 2)
+                          (: (:= 'a (add 'a 1))
+                             (:= 'a (add 'a 1)))
+                          (:= 'b 2))
+                      (止))))
+                   t1-env (list)))
+
+(run (state (list
+             (: (:= 'a 5)
+             (: (:= 'b 50)
+             (: (while (≤ 'a 'b)
+                       (: (:= 'a (add 'a 1))
+                          (:= 'b (sub 'b 1))))
+                (止)))))
+            t1-env (list)))
